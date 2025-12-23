ff35e72ffb303101e80394d2da2430d48e266588
Ola Buildkite <olabuildkite@olabuildkite.local>
2025-12-23 11:30:10 -0700

    Add test script

---
+++ test-script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Simple test script for shellcheck plugin
+echo "Hello from test script"
+echo "Testing shellcheck plugin functionality"
+
+# Simple function
+test_function() {
+    echo "This is a test function"
+}
+
+test
+test_function
+
+exit 0
